6e0d49e565519492c58ee9e18cc94fcd81957d80
Valdemiro <albanoquiquel@gmail.com>
2025-03-24 23:18:31 +0100

    Update models

--- agenda/models.py
+++ agenda/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class Agendamento(models.Model):
     data_horario = models.DateTimeField()
     nome_cliente = models.CharField(max_length = 200)
-    email_cliente = models.CharField(max_length = 200)
+    email_cliente = models.EmailField(max_length = 200)
     telefone = models.CharField(max_length = 20)
